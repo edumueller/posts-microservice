9b84800564e12ba7a0defa11a74adcfbe2842aef
edumueller <eduardo.nicastro@icloud.com>
2021-08-14 14:26:07 -0300

    feat(posts): add create post scaffold

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto')
 
 const PORT = 4000;
 const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
 
 app.get('/posts', (req, res) => {
-
+    res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
-
+app.post('/posts', ({ body: { title } }, res) => {
+    const id = randomBytes(4).toString('hex');
+    posts[id] = { id, title };
+    res.status(201).send(posts[id]);
 });
 
 app.listen(PORT, () => {
